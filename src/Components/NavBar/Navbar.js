3952003f736d8651181cb4f49d03941ef1d0db57
parafeu <muhl.alexandre74@gmail.com>
2019-01-21 16:34:24 +0100

    Debut responsive

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -7,18 +7,39 @@ import Button from '@material-ui/core/Button';
 import './styles.scss';
 
 export default class Navbar extends React.Component{
+
+    state = {
+        width: 0
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            width: window.innerWidth
+        });
+        window.onresize = () => {
+            this.setState({
+                width: window.innerWidth
+            })
+        };
+    };
+
     render(){
         return(
             <div className="nav-container">
                 <AppBar position="fixed" color="primary" className={this.props.transparent ? "transparent" : ""}>
                     <Toolbar >
                         <img id="logo" alt="Logo" src={require('../../assets/imgs/logo.png')}/>
-                        <div className="right">
-                            <Button>Accueil</Button>
-                            <Button>Le restaurant</Button>
-                            <Button>Menu</Button>
-                            <Button>Nos producteurs</Button>
-                        </div>
+                            {
+                                this.state.width > 900 &&
+                                <div className="right">
+                                    <Button>Accueil</Button>
+                                    <Button>Le restaurant</Button>
+                                    <Button>Menu</Button>
+                                    <Button>Nos producteurs</Button>
+                                </div>
+                                }
+                            }
+
                     </Toolbar>
                 </AppBar>
             </div>
