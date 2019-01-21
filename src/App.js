17c21a1368b6aa1b38a69a51cad0331a8314ef09
parafeu <muhl.alexandre74@gmail.com>
2019-01-21 10:12:31 +0100

    Added Material UI and themes variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React, { Component } from 'react';
-import ThemeProvider from './Components/ThemeProvider';
+import React, {Component} from 'react';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import Productor from 'Components/Productor';
+import "typeface-roboto";
 
 class App extends Component {
-  render() {
-    return (
-      <ThemeProvider color="green">
-      </ThemeProvider>
-    );
-  }
+
+    themes = {
+        green: createMuiTheme({
+            palette: {
+                primary: {
+                    light: "#80e27e",
+                    main: "#4caf50",
+                    dark: "#087f23",
+                    contrastText: "#000000"
+                },
+                secondary: {
+                    light: "#cddc39",
+                    main: "#cddc39",
+                    dark: "#99aa00",
+                    contrastText: "#000000"
+                }
+            }
+        }),
+        blue: createMuiTheme({
+            palette: {
+                primary: {
+                    light: "#67daff",
+                    main: "#03a9f4",
+                    dark: "#007ac1",
+                    contrastText: "#000000"
+                },
+                secondary: {
+                    light: "#8bf6ff",
+                    main:"#4fc3f7",
+                    dark: "#0093c4",
+                    contrastText: "#000000"
+                }
+            }
+        })
+    };
+
+    state = {
+        themeColor: "green"
+    };
+
+    render() {
+        return (
+            <MuiThemeProvider theme={this.themes[this.state.themeColor]}>
+                <Productor/>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
