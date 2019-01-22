bd9627bfd8c7c491356b236782816c265d27c5b0
Alexandre MUHL <muhla@iut-acy.local>
2019-01-22 11:14:37 +0100

    Responsive header

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 import './styles.scss';
 
-export default class Navbar extends React.Component{
+export default class Navbar extends React.Component {
 
     state = {
-        width: 0
+        width: 0,
+        drawerOpened: false
     }
+
+    toggleDrawer = () => this.setState({ drawerOpened: !this.state.drawerOpened });
 
     componentDidMount = () => {
         this.setState({
@@ -23,23 +32,59 @@ export default class Navbar extends React.Component{
         };
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="nav-container">
                 <AppBar position="fixed" color="primary" className={this.props.transparent ? "transparent" : ""}>
                     <Toolbar >
-                        <img id="logo" alt="Logo" src={require('../../assets/imgs/logo.png')}/>
-                            {
-                                this.state.width > 900 &&
+                        <img id="logo" alt="Logo" src={require('../../assets/imgs/logo.png')} />
+                        {
+                            this.state.width > 900 && (
                                 <div className="right">
-                                    <Button>Accueil</Button>
-                                    <Button>Le restaurant</Button>
+                                    <Button href="/#">Accueil</Button>
                                     <Button>Menu</Button>
                                     <Button>Nos producteurs</Button>
+                                    <Button>Galerie</Button>
                                 </div>
-                                }
-                            }
+                            )
 
+                        }
+                        {
+                            this.state.width <= 900 && (
+                                <div className="right">
+                                    <IconButton
+                                        onClick={this.toggleDrawer}
+                                    >
+                                        <MenuIcon/>
+                                    </IconButton>
+                                </div>    
+                            )
+                        }
+                        {
+                            this.state.width <= 900 && (
+                                <Drawer 
+                                    open={this.state.drawerOpened}
+                                    onClose={this.toggleDrawer}
+                                    anchor="right"
+                                    color="primary"
+                                >
+                                    <List>
+                                        <ListItem button component="a" href="/#" onClick={this.toggleDrawer}>
+                                            <ListItemText>Accueil</ListItemText>
+                                        </ListItem>
+                                        <ListItem button component="a" href="/#" onClick={this.toggleDrawer}>
+                                            <ListItemText>Menu</ListItemText>
+                                        </ListItem>
+                                        <ListItem button component="a" href="/#" onClick={this.toggleDrawer}>
+                                            <ListItemText>Nos producteurs</ListItemText>
+                                        </ListItem>
+                                        <ListItem button component="a" href="/#" onClick={this.toggleDrawer}>
+                                            <ListItemText>Galerie</ListItemText>
+                                        </ListItem>
+                                    </List>
+                                </Drawer>
+                            )
+                        }
                     </Toolbar>
                 </AppBar>
             </div>
